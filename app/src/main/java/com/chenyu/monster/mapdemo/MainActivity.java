b3f84212f9cdb57c81592c2d803294fe064363fd
王晨宇 <wangchenyu100@gmail.com>
2016-05-16 15:55:24 +0800

    marker点击弹出弹唱覆盖物，点击其它部分则隐藏

--- app/src/main/java/com/chenyu/monster/mapdemo/MainActivity.java
+++ app/src/main/java/com/chenyu/monster/mapdemo/MainActivity.java
@@ -4,11 +4,13 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapPoi;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
@@ -32,22 +34,22 @@ public class MainActivity extends AppCompatActivity {
         BitmapDescriptor bitmap = BitmapDescriptorFactory
                 .fromResource(R.mipmap.logo);
         //构建MarkerOption，用于在地图上添加Marker
-//        MarkerOptions option = new MarkerOptions()
-//                .position(point)//定位显示marker位置
-//                .icon(bitmap)//marker icon
-//                .zIndex(9)//marker所在层级
-//                .draggable(true)//marker是否是可拖拽的
-//                .alpha(0.5f);//marker透明度
-//        option.animateType(MarkerOptions.MarkerAnimateType.grow);//marker使用成长动画
+        MarkerOptions option = new MarkerOptions()
+                .position(point)//定位显示marker位置
+                .icon(bitmap)//marker icon
+                .zIndex(9)//marker所在层级
+                .draggable(true)//marker是否是可拖拽的
+                .alpha(0.5f);//marker透明度
+        option.animateType(MarkerOptions.MarkerAnimateType.grow);//marker使用成长动画
 
         //构建文字覆盖物
-        TextOptions option = new TextOptions()
-                .bgColor(Color.GREEN)
-                .fontSize(60)
-                .fontColor(Color.WHITE)
-                .position(point)
-                .rotate(-30)
-                .text("狗仔");
+//        TextOptions option = new TextOptions()
+//                .bgColor(Color.GREEN)
+//                .fontSize(60)
+//                .fontColor(Color.WHITE)
+//                .position(point)
+//                .rotate(-30)
+//                .text("狗仔");
 
         //弹窗覆盖物
         //创建InfoWindow展示的view
@@ -55,14 +57,6 @@ public class MainActivity extends AppCompatActivity {
         //定义用于显示该InfoWindow的坐标点，这里直接使用point就可以
         //创建InfoWindow，传入view，地理坐标点，y偏移量
         final InfoWindow infoWindow = new InfoWindow(pop, point, -47);
-        //覆盖物marker点击事件中显示infoWindow
-        baiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                baiduMap.showInfoWindow(infoWindow);
-                return false;
-            }
-        });
 
         //在地图上添加Marker，并显示
         baiduMap.addOverlay(option);
@@ -82,26 +76,48 @@ public class MainActivity extends AppCompatActivity {
                 //拖拽开始
             }
         });
+        //覆盖物marker点击事件中显示infoWindow
+        baiduMap.setOnMarkerClickListener(new BaiduMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                baiduMap.showInfoWindow(infoWindow);
+                return false;
+            }
+        });
+        baiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                baiduMap.hideInfoWindow();
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean onMapPoiClick(MapPoi mapPoi) {
+                baiduMap.hideInfoWindow();
+                Toast.makeText(MainActivity.this, "Poi click", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        //在activity执行onPause时执行mMapView.onPause()，实现地图生命周期管理
         mapView.onPause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        //在activity执行onDestroy时执行mMapView. onDestroy ()，实现地图生命周期管理
         mapView.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
 }
